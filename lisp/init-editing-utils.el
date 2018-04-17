3e2f07eef008688babb5ecea6f7e2482f93db497
archieren <renjp_r@hotmail.com>
2018-04-18 05:20:11 +0800

    Little.

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -23,27 +23,28 @@
   (add-hook 'after-init-hook 'electric-pair-mode))
 
 ;; Some basic preferences
-() ;; 显示行号
-(setq-default
- cursor-type 'bar
- blink-cursor-interval 0.4
- bookmark-default-file (expand-file-name ".bookmarks.el" user-emacs-directory)
- buffers-menu-max-size 30
- case-fold-search t
- global-linum-mode t
- column-number-mode t
- delete-selection-mode t
- ediff-split-window-function 'split-window-horizontally
- ediff-window-setup-function 'ediff-setup-windows-plain
- indent-tabs-mode nil
- make-backup-files nil
- mouse-yank-at-point t
- save-interprogram-paste-before-kill t
- scroll-preserve-screen-position 'always
- set-mark-command-repeat-pop t
- tooltip-delay 1.5
- truncate-lines nil
- truncate-partial-width-windows nil)
+(global-linum-mode t)
+(setq-default cursor-type 'bar)
+(setq-default blink-cursor-interval 0.4)
+(setq-default bookmark-default-file (expand-file-name ".bookmarks.el" user-emacs-directory))
+(setq-default buffers-menu-max-size 30)
+(setq-default case-fold-search t)
+(global-linum-mode t)
+(column-number-mode t)
+(delete-selection-mode t)
+(setq-default ediff-split-window-function 'split-window-horizontally)
+(setq-default ediff-window-setup-function 'ediff-setup-windows-plain)
+(setq-default indent-tabs-mode nil)
+(setq-default make-backup-files nil)
+(setq-default mouse-yank-at-point t)
+(setq-default save-interprogram-paste-before-kill t)
+(setq-default scroll-preserve-screen-position 'always)
+(setq-default set-mark-command-repeat-pop t)
+(setq-default tooltip-delay 1.5)
+(setq-default truncate-lines nil)
+(setq-default truncate-partial-width-windows nil)
+
+
 
 (add-hook 'after-init-hook 'global-auto-revert-mode)
 (setq global-auto-revert-non-file-buffers t
