e1229ac757f3d31e031ffd227d1db19cd28ee591
archieren <renjp_r@hotmail.com>
2018-04-17 18:18:21 +0800

    Nothing!

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -34,11 +34,7 @@
 (window-number-mode)
 (window-number-meta-mode)
 
-;;; 设置Smex
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
-;; This is your old M-x.
-(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
 
 ;;; 设置 multiple-cursors
 (require-package 'multiple-cursors)
---
+++ lisp/init-recentf.el
@@ -0,0 +1,11 @@
+;;; init-recentf --- Nothing
+;;; Commentary:
+;;; Code:
+(add-hook 'after-init-hook 'recentf-mode)
+(setq-default
+ recentf-max-saved-items 1000
+ recentf-exclude '("/tmp/" "/ssh:"))
+
+
+(provide 'init-recentf)
+;;; init-recentf ends here
