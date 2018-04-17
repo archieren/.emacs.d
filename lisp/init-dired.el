513b3943ff85e9ea378cdfdd05ea3a96dc9211af
archieren <renjp_r@hotmail.com>
2018-04-17 16:18:30 +0800

    Only one dired-buffer!

---
+++ lisp/init-dired.el
@@ -0,0 +1,31 @@
+;;; init-dired --- Nothing
+;;; Commentary:
+;;; Code:
+
+(setq-default dired-dwim-target t)
+
+;; Prefer g-prefixed coreutils version of standard utilities when available
+(let ((gls (executable-find "gls")))
+  (when gls (setq insert-directory-program gls)))
+
+(when (maybe-require-package 'diredfl)
+  (after-load 'dired
+    (diredfl-global-mode)))
+
+;; 只在一个buffer 里打开 dired-mode!
+(put 'dired-find-alternate-file 'disabled nil)
+(after-load 'dired
+  (setq dired-recursive-deletes 'top)
+  (define-key dired-mode-map [mouse-2] 'dired-find-alternate-file)
+  (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file)
+  (define-key dired-mode-map (kbd "C-c C-p") 'wdired-change-to-wdired-mode)
+  (add-hook 'dired-mode-hook
+            (lambda () (guide-key/add-local-guide-key-sequence "%"))))
+
+(when (maybe-require-package 'diff-hl)
+  (after-load 'dired
+    (add-hook 'dired-mode-hook 'diff-hl-dired-mode)))
+
+
+(provide 'init-dired)
+;;; init-dired ends here
