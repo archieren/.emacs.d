eb1230f4b6922e549423a4a23b9ce6540d63d24b
archieren <renjp_r@hotmail.com>
2020-11-08 22:12:31 +0800

    ......

--- init.el
+++ init.el
@@ -234,8 +234,8 @@ Should Not be too big." )
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t
         doom-themes-enable-italic t)
-  ;; (load-theme `doom-solarized-dark t)
-  (load-theme `doom-one t)
+  (load-theme `doom-solarized-dark t)
+  ;; (load-theme `doom-one t)
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
   ;; Corrects (and improves) org-mode's native fontification.
@@ -1245,3 +1245,4 @@ Eval region from begin-mark to end-mark if active, otherwise the last sexp."
 (provide `init)
 ;;; init.el ends here
 
+(put 'downcase-region 'disabled nil)
